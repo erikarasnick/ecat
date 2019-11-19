f39cb06443ba9c6955fc0d2d3422cf075864b0a6
Erika Rasnick <erika.rasnick@gmail.com>
2019-11-19 12:17:07 -0500

    change readme fig

--- man/figures/create_temporal_variability_fig.R
+++ man/figures/create_temporal_variability_fig.R
@@ -2,10 +2,10 @@ library(tidyverse)
 library(ecat)
 
 d <- tibble::tibble(id = rep(809089L,12),
-                    lon = rep(-84.69127387,12),
-                    lat = rep(39.24710734,12),
-                    start_date = seq.Date(as.Date("2010-11-08"), as.Date("2011-10-08"), by="month"),
-                    end_date = seq.Date(as.Date("2010-12-08"), as.Date("2011-11-08"), by="month"))
+                    lon = rep(-84.703717,12),
+                    lat = rep(39.035665,12),
+                    start_date = seq.Date(as.Date("2003-06-03"), as.Date("2004-05-03"), by="month"),
+                    end_date = seq.Date(as.Date("2003-07-03"), as.Date("2004-06-03"), by="month"))
 
 
 d %>%
