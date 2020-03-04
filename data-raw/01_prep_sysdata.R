ada2bebb1c17a48e0281ea09e5b2501c7838fd3f
Erika <erika.rasnick@gmail.com>
2020-03-04 14:53:48 -0500

    crop elevation raster to 7-co area

--- data-raw/01_prep_sysdata.R
+++ data-raw/01_prep_sysdata.R
@@ -1,8 +1,15 @@
-elevation.raster <- readRDS('./data-raw/elevation_raster.rds')
 interstate.lines.sf <- readRDS('./data-raw/interstate_lines_sf.rds')
 highway.lines.sf <- readRDS('./data-raw/highway_lines_sf.rds')
 bus.route.lines.sf <- readRDS('./data-raw/bus_route_lines_sf.rds')
 ec_ts <- readRDS("./data-raw/ec_ts.rds")
+
+# elevation.raster <- readRDS('./data-raw/elevation_raster.rds')
+# grid <- sf::st_read('./data-raw/prediction_grid_1km/prediction_grid_1km.shp')
+# e_crop <- raster::crop(e, raster::extent(grid)) %>%
+#   raster::mask(grid)
+# saveRDS(e_crop, "elevation_raster_trimmed.rds")
+
+elevation.raster <- readRDS('./data-raw/elevation_raster_trimmed.rds')
 
 usethis::use_data(elevation.raster,
                   interstate.lines.sf,
